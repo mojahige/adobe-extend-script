7bbeab0ee1783f0c80afec7d1922db90a83bbf38
mojahige <mojahige@gmail.com>
2020-12-05 08:41:17 +0900

    レイヤーを動かすところで詰まった
    なぜかレイヤーの中身が行方不明になる

--- packages/ps-set-copyright/src/decls.d.ts
+++ packages/ps-set-copyright/src/decls.d.ts
@@ -1,6 +1,94 @@
-interface App {
-  activeDocument: { [key: string]: any };
-  currentTool: string;
-  documents: Array;
+declare const NewDocumentMode;
+declare const DocumentFill;
+declare const ElementPlacement;
+declare const Units;
+declare const OffsetUndefinedAreas;
+
+type UnitValue = string;
+
+type ElementPlacement = unknown;
+
+interface ArtLayer {
+  allLocked: boolean;
+  isBackgroundLayer: boolean;
+  name: string;
+  bounds: UnitValue[],
+  move: (
+    relativeObject: ArtLayer | LayerSet,
+    insertionLocation)
+  => void;
+  translate: (deltaX: number, deltaY: number) => void;
+  applyOffset: (horizontal: UnitValue, vertical: UnitValue, undefinedAreas: OffsetUndefinedAreas);
 }
+
+interface ArtLayers {
+  [key: number]: ArtLayer;
+  length: number;
+  parent: Document;
+  typeName: String;
+}
+
+interface LayerSet {
+  allLocked: boolean;
+  artLayers: ArtLayers;
+}
+
+interface Layers {
+  [key: number]: ArtLayer | LayerSet;
+  length: number;
+  parent: Document | LayerSet;
+  typename: string;
+}
+
+interface Selection {
+  copy: (merge?: boolean) => void;
+  deselect: () => void;
+  select: (
+    region: number[],
+    type: unknown,
+    feather: number,
+    antiAlias: boolean
+  ) => void;
+  selectAll: () => void;
+}
+
+interface Document {
+  activeChannels: [];
+  activeHistoryBrushSource: unknown;
+  activeLayer: LayerSet | ArtLayer;
+  width: UnitValue;
+  height: UnitValue;
+  selection: Selection;
+  paste: () => ArtLayer;
+  artLayers: ArtLayers;
+  name: string; 
+  layers: Layers;
+}
+
+type Documents = {
+  [key: number]: Document;
+  parent: App;
+  length: number;
+  typename: string;
+  add: (
+    width?: UnitValue,
+    height?: UnitValue,
+    resolution?: number,
+    name?: string,
+    mode?: unknown,
+    initialFill?: unknown,
+    pixelAspectRatio?: number,
+    bitsPerChannel?: unknown,
+    colorProfileName?: string
+  ) => Document;
+}
+
+interface App {
+  activeDocument: Document;
+  documents: Documents;
+  preferences: {
+    rulerUnits: Units;
+  };
+}
+
 declare var app: App;
--- packages/ps-set-copyright/src/main.ts
+++ packages/ps-set-copyright/src/main.ts
@@ -1,19 +1,53 @@
-function getDocument() {
+interface DocumentData {
+  copySourceDocument: Document | undefined;
+  pasteToDocuments: Document[];
+}
+
+interface GenerateDocumentArguments {
+  baseDocument: Document;
+  sourceDocument: Document;
+}
+
+interface SplitFileNameResult {
+  name: string | undefined;
+  extension: string | undefined;
+}
+
+interface AddDocumentArguments {
+  width: UnitValue;
+  height: UnitValue;
+  fileName: string;
+  fileExtension: string | undefined;
+}
+
+interface MoveLayerArguments {
+  document: Document;
+  moveLayer: ArtLayer;
+}
+
+function splitToFileNameAndExtension(documentName: string): SplitFileNameResult {
+  const result = documentName.split(/\.(?=[^.]+$)/)
+
+  return {
+    name: result[0],
+    extension: result[1]
+  }
+}
+
+function getDocument(): DocumentData {
   const documents = app.documents
   const copySourceImageIdentifierString = 'copyright'
-  let copySourceDocument = undefined
-  let pasteToDocuments = []
+  let copySourceDocument: Document | undefined = undefined
+  let pasteToDocuments: Document[] = []
 
-  for (var i = 0; i < documents.length; i++) {
-    const { name, width, height } = documents[i]
+  for (let i = 0; i < documents.length; i++) {
+    const { name } = documents[i]
 
     if (name.indexOf(copySourceImageIdentifierString) > -1) {
       copySourceDocument = documents[i]
     } else {
       pasteToDocuments.push(documents[i])
     }
-
-    alert(`"${name}" size 👉 ${width} x ${height}` )
   }
 
   return {
@@ -22,10 +56,128 @@ function getDocument() {
   }
 }
 
-function run() {
-  const { copySourceDocument, pasteToDocuments } = getDocument()
-
-  console.log(copySourceDocument, pasteToDocuments)
+function activateDocument(document: Document): void {
+  app.activeDocument = document;
 }
 
-run();
+function selectionCopy(baseDocument: Document): void {
+  activateDocument(baseDocument)
+
+  if (
+    (baseDocument.activeLayer as ArtLayer).isBackgroundLayer != undefined
+  ) {
+    (baseDocument.activeLayer as ArtLayer).isBackgroundLayer = false
+  }
+  
+  baseDocument.activeLayer.allLocked = false
+  baseDocument.selection.selectAll()
+  baseDocument.selection.copy(true)
+  baseDocument.selection.deselect()
+}
+
+function addDocument({
+  width,
+  height,
+  fileName
+}: AddDocumentArguments): Document {
+  return app.documents.add(
+    width,
+    height,
+    1.0,
+    `${fileName}_modify`,
+    NewDocumentMode.RGB,
+    DocumentFill.TRANSPARENT
+  )
+}
+
+function translateLayer({
+  document,
+  moveLayer
+}: MoveLayerArguments): void {
+  activateDocument(document)
+
+  // const { width, height } = document;
+
+  // Move the layer to the forefront
+  moveLayer.move(
+    document.layers[0],
+    ElementPlacement.PLACEBEFORE
+  )
+
+  const [top, left] = moveLayer.bounds
+
+  moveLayer.translate(0, 0)
+}
+
+function createNewDocument({
+  baseDocument,
+  sourceDocument
+}: GenerateDocumentArguments) {
+  const {
+    name: baseDocumentName,
+    width,
+    height
+  } = baseDocument;
+  const {
+    name: fileName,
+    extension: fileExtension,
+  } = splitToFileNameAndExtension(baseDocumentName)
+
+  selectionCopy(baseDocument)
+
+  const newDocument = addDocument({
+    width,
+    height,
+    fileName,
+    fileExtension
+  })
+
+  newDocument.paste()
+  selectionCopy(sourceDocument)
+  activateDocument(newDocument)
+
+  const sourcePasteLayer = newDocument.paste()
+
+  translateLayer({ document: newDocument, moveLayer: sourcePasteLayer })
+
+  return newDocument;
+}
+
+function seve({
+  baseDocument,
+  sourceDocument
+}: GenerateDocumentArguments): void {
+  createNewDocument({ baseDocument, sourceDocument });
+}
+
+function generateNewImage({ copySourceDocument, pasteToDocuments }: DocumentData) {
+  for (let i = 0; i < pasteToDocuments.length; i++) {
+    seve({
+      baseDocument: pasteToDocuments[i],
+      sourceDocument: copySourceDocument
+    })
+  }
+}
+
+function run() {
+  app.preferences.rulerUnits = Units.PIXELS;
+
+  const { copySourceDocument, pasteToDocuments } = getDocument()
+
+  if (copySourceDocument == undefined) {
+    alert('🆖\nコピー元となるファイルが見つかりませんでした\n命名ルールについて確認してください🦖')
+  }
+
+  if (!pasteToDocuments.length) {
+    alert('🆖\nペースト先のファイルが見つかりませんでした\nなんでだろ？\nファイルがなかったのかもしれないね🦖');
+  }
+
+  generateNewImage({ copySourceDocument, pasteToDocuments });
+}
+
+try {
+  run();
+} catch(error) {
+  alert('💣\n予期せぬエラーが発生しました\n\n' + error)
+}
+
